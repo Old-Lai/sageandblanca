32a06c2190762da20949015e8da914dacb3d5409
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-09-16 14:21:18 -0700

    added temperary mobile view menu panel and buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,31 @@
-import { Menu } from 'lucide-react';
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="flex h-full w-16 items-center justify-center bg-gray-600/0">
-      <Menu />
+    <div className="h-full w-16 bg-gray-600/0">
+      <button className="h-full w-full flex items-center justify-center" onClick={()=>toggleNavPanel()}>
+        <Menu id="navHambergerIcon" className="z-10"/>
+        <X id="navHambergerCloseIcon" className="z-10 hidden"/>
+      </button>
+      <div id="navPanel" className="absolute left-0 top-0 hidden h-screen w-screen flex flex-col items-center justify-center bg-slate-700">
+        <button className="w-full h-14" onClick={()=>toggleNavPanel()}>
+          <Link to="/" className="text-4xl">Home</Link>
+        </button>
+        <button className="w-full h-14">
+          <Link to="/order" className="text-4xl" onClick={()=>toggleNavPanel()}>Order</Link>
+        </button>
+      </div>
     </div>
   );
+}
+
+function toggleNavPanel(){
+  let navPanel = document.getElementById("navPanel")
+  let navHambergerIcon = document.getElementById("navHambergerIcon")
+  let navHambergerCloseIcon = document.getElementById("navHambergerCloseIcon")
+
+  navPanel && navPanel.classList.toggle("hidden")
+  navHambergerIcon && navHambergerIcon.classList.toggle("hidden")
+  navHambergerCloseIcon && navHambergerCloseIcon.classList.toggle("hidden")
 }
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,20 +1,27 @@
-import { Outlet } from "react-router-dom"
-import { NavBar } from "../components"
-import { ShoppingCart } from 'lucide-react';
+import { Outlet } from "react-router-dom";
+import { NavBar } from "../components";
+import { ShoppingCart } from "lucide-react";
+import { useEffect, useState } from "react";
 
-
-export default function Root(){
-    return(
-        <>
-            <header className="bg-orange-300 w-full h-16 flex justify-between items-center">
-                <NavBar/>
-                <h1 className="flex-1 text-center">Shop Name</h1>
-                <ShoppingCart className="w-16"/>
-            </header>
-            <main>
-                <Outlet/>
-            </main>
-            <footer></footer>
-        </>
-    )
+export default function Root() {
+  const [atTop, setAtTop] = useState(true);
+  const shrinkNavbar = () =>
+    window.scrollY >= 80 ? setAtTop(false) : setAtTop(true);
+  window.addEventListener("scroll", shrinkNavbar);
+  useEffect(() => {
+    console.log(window.location.pathname)
+  }, [window.location.pathname])
+  return (
+    <>
+      <header className=" sticky left-0 top-0 flex h-16 w-full items-center justify-between bg-zinc-700/55 text-white">
+        <NavBar />
+        <h1 className="z-10 flex-1 text-center text-3xl font-medium">Shop Name</h1>
+        <ShoppingCart className="z-10 w-16" />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
+  );
 }
