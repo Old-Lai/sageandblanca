f461f024dd8d1525cf3d48e06334011df4e12881
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-09-16 16:10:03 -0700

    added sections for banner + section under for products display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
-import banner from "@/assets/temp/temp-banner.jpeg"
+import banner from "@/assets/temp/temp-banner.jpeg";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <img src={banner} className="h-screen"/>
-      </div>
-      <p>test</p>
+      <section className="flex h-[95vh] items-center justify-center bg-gray-800">
+        <img
+          src={banner}
+          className="absolute left-0 top-0 h-[95vh] w-full object-cover"
+        />
+        <h1 className="z-10 text-[2.9rem] font-bold text-white">
+          Bespoke Blooms
+        </h1>
+      </section>
+      <section className="mx-4 my-20 flex items-center justify-center">
+        <section>
+          <h1 className="p-5 text-center text-3xl font-semibold">
+            An expression of the season
+          </h1>
+          <p className="text-center text-lg font-light">
+            Each bouquet is an one-of-a-kind creation. Simply pick a size and
+            color tone and we'll do the magic. Freshest ingredients, creativity
+            and suprises are guaranteed.
+          </p>
+        </section>
+
+      </section>
     </>
   );
 }
