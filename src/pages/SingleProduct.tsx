b320e538bdda08143cf5eda5055163428bb4c3b8
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-09-19 23:35:49 -0700

    changed temp json format test grab in SingleProduct page

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,12 +1,36 @@
+import banner from "@/assets/temp/temp-banner.jpeg";
+import image2 from "@/assets/temp/temp-image.jpg";
 import { useParams } from "react-router-dom";
 import { ImageCarosel } from "@/components";
+import products from "@/assets/temp/productTest.json";
+
+interface Product {
+    name: string,
+    dollar: string,
+    cent: string,
+    image: string,
+}
 
 export default function SingleProduct() {
-  const { id } = useParams();
+  let { id: productId }  = useParams();
+  const images = [
+    { url: banner, alt: "Image 1" },
+    { url: image2, alt: "Image 2" },
+    { url: banner, alt: "Image 3" },
+    // Add more images here...
+  ];
 
+  productId = productId? productId : "0";
+  
+  let product: Product = products["1"];
+
+  console.log(product)
   return (
     <div className="my-20 mx-5">
-      <ImageCarosel />
+      <ImageCarosel images={images}/>
+      <div>
+        
+      </div>
     </div>
   );
 }
