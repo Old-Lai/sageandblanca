32614852d872a15bf62a55911219cc9e8ab7971d
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-09-24 16:27:31 -0700

    revert import path back to without .tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as NavBar } from "./NavBar.tsx";
-export { default as ProductCard } from "./ProductCard.tsx";
-export { default as OrderInquiryForm } from "./OrderInquiryForm.tsx";
-export { default as ProductImageList } from "./ProductImageList.tsx";
-export { default as FooterNav } from "./FooterNav.tsx";
-export { default as ImageCarosel } from "./ImageCarosel.tsx";
+export { default as NavBar } from "./NavBar";
+export { default as ProductCard } from "./ProductCard";
+export { default as OrderInquiryForm } from "./OrderInquiryForm";
+export { default as ProductImageList } from "./ProductImageList";
+export { default as FooterNav } from "./FooterNav";
+export { default as ImageCarosel } from "./ImageCarosel";
