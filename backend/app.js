b22c2b3057516aa48a5722f6b3050b193c6e4ea0
Henry <89488963+Old-Lai@users.noreply.github.com>
2024-10-04 15:39:22 -0700

    added async function to only run code after database connection establshed

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-try {
-  async function ConnectPool() {
+async function ConnectPool() {
+  try {
+    console.log("Connecting to database..........");
     await pool.connect();
+    console.log("Connected to database server");
+  } catch (error) {
+    console.error("Failed to connect to database server", error);
+    throw error; //prvent code from runnin if no connection
   }
-
-  ConnectPool();
-  console.log("Connected to database server");
-} catch (error) {
-  console.error("Failed to connect to database server", error);
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+  });
 }
+
+ConnectPool();
 
 app.use((err, req, res, next) => {
   res.status(500).next({
@@ -22,9 +28,4 @@ app.use((err, req, res, next) => {
     name: err.name,
     message: err.message,
   });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
 });
