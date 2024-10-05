5f683fd399648d1f06283ece41ed151bf4a627cd
Old-Lai <henrylai8115@gmail.com>
2024-10-05 16:57:19 -0700

    added drop and create table for media and media placements

---
+++ backend/src/seed.js
@@ -0,0 +1,60 @@
+const client = require("./client");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS medias;
+      DROP TABLE IF EXISTS media_placements;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (e) {
+    console.error("Error dropping tables!!!");
+    throw e;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`  
+      CREATE TABLE medias(
+        internal_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+        url VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255) NOT NULL
+      );
+
+      CREATE TABLE media_placements(
+        media_id UUID REFERENCES medias(internal_id) NOT NULL,
+        placement VARCHAR(255) NOT NULL,
+        index INTEGER UNIQUE NOT NULL,
+        id_active BOOLEAN DEFAULT TRUE
+      );
+  `);
+
+    console.log("Finished building tables!");
+  } catch (e) {
+    console.error("Error building tables!!!");
+    throw e;
+  }
+}
+
+async function seed() {
+  try {
+    await dropTables();
+    await createTables();
+    console.log("Database seeded successfully!");
+  } catch (e) {
+    console.error("Error seeding database!!!");
+    throw e;
+  } finally {
+    client.end();
+  }
+}
+
+if (require.main === module) {
+  seed();
+}
