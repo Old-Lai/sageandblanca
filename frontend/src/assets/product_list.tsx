085d61608a577780e05f55c2d24ed4a2e0ec0e64
Henry <89488963+Old-Lai@users.noreply.github.com>
2025-01-08 10:06:18 -0800

    added correct pricing

--- frontend/src/assets/product_list.tsx
+++ frontend/src/assets/product_list.tsx
@@ -3,27 +3,27 @@ import arragement_img from "@/assets/images/arrangement/arrangement-1.jpeg"
 import rose_img from "@/assets/images/rose/rose-1.jpeg"
 interface Product {
   name: string;
-  dollar: string;
-  cent: string;
+  dollar: number;
+  cent: number;
   image: string;
 }
 export const productList: Record<string, Product> = {
   Bespoke_Bouquet: {
     name: "Bespoke Bouquet",
-    dollar: "85",
-    cent: "00",
+    dollar: 85,
+    cent: 0,
     image: bouquet_img,
   },
   Bespoke_Arrangement: {
     name: "Bespoke Arrangement",
-    dollar: "85",
-    cent: "00",
+    dollar: 85,
+    cent: 0,
     image: arragement_img,
   },
   Rose_Bouquet: {
     name: "Rose Bouquet",
-    dollar: "70",
-    cent: "00",
+    dollar: 70,
+    cent: 0,
     image: rose_img,
   },
 };
