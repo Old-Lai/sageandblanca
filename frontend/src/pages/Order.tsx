2cab61a9c474b5a6850c6e55336adf8fdf0be432
Henry <89488963+Old-Lai@users.noreply.github.com>
2025-01-08 10:09:47 -0800

    used productList json for productCard

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,21 +1,26 @@
-import banner from "@/assets/temp/temp-banner.jpeg";
 import { ProductCard } from "@/components";
+import { productList } from "@/assets/product_list";
+
 export default function Order() {
   return (
     <div className="mt-20 w-full">
       <div className="mb-10 flex w-full flex-wrap justify-between px-5">
-        {Array.from({ length: 3 }).map((_, index) => (
-            <ProductCard
-              size="medium"
-              image={banner}
-              name={`Bouquet ${index + 1}`}
-              dollar={90 + index}
-              cent={index + 1}
-              productId={`${index}`}
-              key={"product-card-" + index.toString()}
-             
-            />
-        ))}
+        {Object.keys(productList).map((productKey, index) => {
+          const product = productList[productKey as keyof Object];
+          return (
+            <div key={"product-card-" + index.toString()} className="flex flex-col">
+              <ProductCard
+                size="medium"
+                image={product.image}
+                name={`${product.name}`}
+                dollar={product.dollar}
+                cent={product.cent}
+                productId={`${index}`}
+              />
+              <button className="bg-gray-200 py-2 mb-4 rounded-md">Order</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
