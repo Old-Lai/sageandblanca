bd2f3247ff1f9bfeee3fa469cb5974eb48c0220b
Henry <89488963+Old-Lai@users.noreply.github.com>
2025-01-08 10:37:33 -0800

    add order button navigation on click

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,14 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { ProductCard } from "@/components";
 import { productList } from "@/assets/product_list";
 
 export default function Order() {
+  const navigate = useNavigate();
   return (
     <div className="mt-20 w-full">
       <div className="mb-10 flex w-full flex-wrap justify-between px-5">
         {Object.keys(productList).map((productKey, index) => {
           const product = productList[productKey as keyof Object];
           return (
-            <div key={"product-card-" + index.toString()} className="flex flex-col">
+            <div
+              key={"product-card-" + index.toString()}
+              className="flex flex-col"
+            >
               <ProductCard
                 size="medium"
                 image={product.image}
@@ -17,7 +22,14 @@ export default function Order() {
                 cent={product.cent}
                 productId={`${index}`}
               />
-              <button className="bg-gray-200 py-2 mb-4 rounded-md">Order</button>
+              <button
+                className="mb-4 rounded-md bg-gray-200 py-2"
+                onClick={() => {
+                  navigate(`/product/${index}`);
+                }}
+              >
+                Order
+              </button>
             </div>
           );
         })}
